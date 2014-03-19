d2107ff3d2f24f79d34f45d7ea54603fadac1b76
Faezeh Dorri <fdorri@sumac.umiacs.umd.edu>
2014-03-19 14:00:47 -0400

    Simulatorbugfix

--- simulator/simulator.cpp
+++ simulator/simulator.cpp
@@ -1,4 +1,4 @@
-// to be run : 
+// to be run :
 //./Simulate < input.in > /cbcb/project-scratch/fdorri/Code/methylFlow/testing/test0.tsv
 #include "simulator.h"
 #include <fstream>
@@ -8,49 +8,49 @@ std::ofstream patternFile;
 
 
 void simulator::readData(){
-
-		cin >> dnaLength >> readLength >> HapNum >> coverage >> error >> flag;
-        if(flag > 0){
-            int sum = 0;
-            for( int i=0; i < HapNum; i++){
-                freq.push_back(rand() %100);
-                sum += freq[i];
-            }
-            for(int i =0; i<HapNum; i++){
-                freq[i] = freq[i]*100/sum;
-            }
-            
-            bool* A = new bool[dnaLength];
-            for (int i=0; i<dnaLength; i++)
-                A[i] = false;
-            
-            for(int i =0; i < flag; i++){
-                int p = rand() % dnaLength;
-                A[p] = true;
-            }
-            for (int i=0; i< dnaLength; i++) {
-                if (A[i])
-                    pos.push_back(i);
-            }
-                
-        
+    
+    cin >> dnaLength >> readLength >> HapNum >> coverage >> error >> flag;
+    if(flag > 0){
+        int sum = 0;
+        for( int i=0; i < HapNum; i++){
+            freq.push_back(rand() %100);
+            sum += freq[i];
         }
-        else{
-            for(int i=0; i < HapNum; i++){
-                int f;
-                cin >> f;
-                freq.push_back(f);
-            }
-            int posNum;
-            cin >> posNum;
-            for(int i=0; i < posNum; i++){
-                int p;
-                cin >> p;
-                pos.push_back(p);
-            }
+        for(int i =0; i<HapNum; i++){
+            freq[i] = freq[i]*100/sum;
+        }
+        
+        bool* A = new bool[dnaLength];
+        for (int i=0; i<dnaLength; i++)
+            A[i] = false;
+        
+        for(int i =0; i < flag; i++){
+            int p = rand() % dnaLength;
+            A[p] = true;
+        }
+        for (int i=0; i< dnaLength; i++) {
+            if (A[i])
+                pos.push_back(i);
         }
         
         
+    }
+    else{
+        for(int i=0; i < HapNum; i++){
+            int f;
+            cin >> f;
+            freq.push_back(f);
+        }
+        int posNum;
+        cin >> posNum;
+        for(int i=0; i < posNum; i++){
+            int p;
+            cin >> p;
+            pos.push_back(p);
+        }
+    }
+    
+    
 }
 
 void simulator::buildMethylHap(int dnaLength, vector<int> pos, int HapNum, vector<MethylHap>& methylHapVec){
@@ -107,7 +107,7 @@ void simulator::selectHP(int readLength, int dnaLength, vector<int> freq, int& h
 }
 
 void simulator::buildRead(int hap, int pos,int readLength, int error, vector<MethylHap> methylHapVec, MethylRead& read){
-
+    
     read.start = pos;
     read.length = readLength;
     //cout << "hap: " << hap << endl;
@@ -127,19 +127,21 @@ void simulator::buildRead(int hap, int pos,int readLength, int error, vector<Met
         }
         if(methylHapVec[hap].methyl[i].pos > pos + readLength)
             break;
-            
+        
     }
 }
 
 void simulator::writeMethylRead(MethylRead read, int k){
 	
     cout << "read" << k <<  "\t" << read.start << "\t" << read.length << "\t" << "W" << "\t";
-    for(unsigned int i=0; i< read.methyl.size()-1; i++){
-        cout << read.methyl[i].pos - read.start  << ":" << read.methyl[i].type << ",";
-    }
-    cout << read.methyl[read.methyl.size()-1].pos - read.start  << ":" << read.methyl[read.methyl.size()-1].type;
-	if(read.methyl.size()==0)
+    if(read.methyl.size()==0)
 		cout << "*" ;
+    else{
+        for(unsigned int i=0; i< read.methyl.size()-1; i++){
+            cout << read.methyl[i].pos - read.start  << ":" << read.methyl[i].type << ",";
+        }
+        cout << read.methyl[read.methyl.size()-1].pos - read.start  << ":" << read.methyl[read.methyl.size()-1].type;
+    }
     cout << "\t" << "MismatchData" << endl;
 }
 
@@ -147,25 +149,27 @@ void simulator::writeMethylRead(MethylRead read, int k){
 void simulator::simulate(){
 	vector<MethylRead> data;
     readData();
-    //cout << "read Done" << endl;
+    cout << "read Done" << endl;
     buildMethylHap(dnaLength, pos, HapNum, methylHapVec);
-    //cout << "hap built" << endl;
+    cout << "hap built" << endl;
     for(int i=0; i < coverage*dnaLength/readLength; i++){
         int hap=0;
         int pos=0;
         MethylRead read;
         selectHP(readLength, dnaLength, freq, hap, pos);
-      //  cout << "HP Seleced " << i << endl;
-       // cout << "hap " << hap << endl;
-        //cout << "pos " << pos << endl;
+        cout << "HP Seleced " << i << endl;
+        cout << "hap " << hap << endl;
+        cout << "pos " << pos << endl;
         buildRead(hap, pos, readLength, error, methylHapVec, read);
 		data.push_back(read);
 		
-        //cout << "read Wrote" << endl;
+        cout << "read Wrote" << endl;
     }
 	sort(data.begin(), data.end());
-    //cout << "Read built" << endl; 
+    cout << "Read built" << endl;
+    cout << data[30].methyl.size() << endl;
 	for(unsigned int i=0; i <data.size(); i++){
+        cout << i;
 		writeMethylRead(data[i], i);
 	}
 }
